2acdc9fef86030f5d62e7c5972cec7a6dfc1b817
Adam Fousek <fousek@atlasgroup.cz>
2022-01-05 08:04:15 +0100

    WIP: React context

---
+++ assets/js/contexts/TodoContext.js
@@ -0,0 +1,29 @@
+import React, {createContext, useState} from 'react';
+
+export const TodoContext = createContext();
+
+function TodoContextProvider(props) {
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = () => {};
+
+  const readTodo = () => {};
+
+  const updateTodo = () => {};
+
+  const deleteTodo = () => {};
+
+  return (
+    <TodoContext.Provider value={{
+      ...todos,
+      createTodo,
+      readTodo,
+      updateTodo,
+      deleteTodo,
+    }}>
+      {props.children}
+    </TodoContext.Provider>
+  );
+}
+
+export default TodoContextProvider;
